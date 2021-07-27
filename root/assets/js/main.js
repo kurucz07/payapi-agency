483ea679b0788770a0e138c48e5f06aa1226a0b7
Israel Adefidipe <israeladefidipe@gmail.com>
2021-07-27 21:53:28 +0100

    Header: Drop-down menu done

---
+++ root/assets/js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// DOM element variables
+const btnHamburger = document.querySelector('#btnHamburger');
+const nav = document.querySelector('.nav')
+
+
+// Dropdown functionality
+btnHamburger.addEventListener('click', function(){
+    this.classList.toggle('header__menu--active');
+    nav.classList.toggle('nav--active');
+
+})
